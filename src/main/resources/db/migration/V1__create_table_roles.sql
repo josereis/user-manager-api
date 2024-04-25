ece8668ff5cf437c21bd9acc8cb137770672b150
= <jose.santiago@maida.health>
2024-04-25 11:44:39 -0300

    feat: Adding the record of the first roles to be added after creating tb_roles.

--- src/main/resources/db/migration/V1__create_table_roles.sql
+++ src/main/resources/db/migration/V1__create_table_roles.sql
@@ -20,4 +20,9 @@ CREATE TABLE IF NOT EXISTS public.tb_roles
     pretty_name character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT tb_roles_pkey PRIMARY KEY (id),
     CONSTRAINT tb_roles_uuid_key UNIQUE (uuid)
-)
+);
+
+INSERT INTO public.tb_roles (active,deleted,created_at,updated_at,"version",uuid,description,name,pretty_name) VALUES
+	 (true,false,current_timestamp,current_timestamp,0,'60febc0c-f421-4b52-b540-4f7f797ddbe1','Owner','OWNER','Owner'),
+	 (true,false,current_timestamp,current_timestamp,0,'5dcb5cf5-578d-4452-8dac-92676420070f','Administrator','ADMIN','Admin');
+;
